e49cd3ecb9a684942f41d750c1f75d14b84064ac
patilchetana123 <chetupatel89@gmail.com>
2022-12-13 09:22:48 +0530

    first commit

---
+++ src/Example/Array.java
@@ -0,0 +1,27 @@
+package Example;
+
+public class Array {
+
+	public static void main(String[] args) 
+	{
+        int []show=new int[5];
+		
+		show[0]=10;
+		show[1]=20;
+		show[2]=30;
+		show[3]=40;
+		show[4]=50;
+				
+
+		System.out.println(show[0]);
+		System.out.println(show[1]);
+		System.out.println(show[2]);
+		System.out.println(show[3]);
+		System.out.println(show[4]);
+
+		
+		
+
+	}
+
+}
